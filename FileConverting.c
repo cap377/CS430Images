5e17b4e1ffa11601ff25e8032429f05f085dd746
Cherie Parsons <cap377@nau.edu>
2016-09-14 23:21:35 -0700

    added the header scanning and comment ignoring functionality, along with an attempt at pixel reading

--- FileConverting.c
+++ FileConverting.c
@@ -5,36 +5,96 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+typedef struct Pixel{
+	int red;
+	int green;
+	int blue;
+} Pixel;
+
+FILE *input_fp, *output_fp;
+int height, width, depth;
+
+void p6_to_p3(FILE *input, FILE *output, Pixel *buffer);
+FILE* escape_comments(FILE *input, int c);
+
 int main(int argc, char** argv){
 	
-	FILE *fp;
-	int i, size;
-	char c;
+	int size;
+	int c;
 	
-	fp = fopen("test.txt", "rb");
-	if(fp == NULL){
+	input_fp = fopen("upraw.ppm", "rb");
+	if(input_fp == NULL){
 		perror("File was not found");
 	}
+	output_fp = fopen("something.ppm", "wb");
+	if(output_fp == NULL){
+		perror("Output file failed in creation");
+	}
 	
-	fseek(fp, 0, SEEK_END);
-	size = ftell(fp);
+	fseek(input_fp, 0, SEEK_END);
+	size = ftell(input_fp);
 	printf("Size of file: %i\n", size);
-	fseek(fp, 0, SEEK_SET);
+	fseek(input_fp, 0, SEEK_SET);
 	
-	char *buffer = malloc(size+1);
+	Pixel *buffer = malloc(size+1);
 	
-	/*c = fgetc(fp);
+	c = fgetc(input_fp);
 	if(c != 80){
 		printf("Incorrect file type: Expected .PPM file\n");
 		return 1;
 	}
-	fseek(fp, 0, SEEK_SET);*/
 	
-	for(i = 0; i < size; i++){
-		c = fgetc(fp);
-		buffer[i] = c;
+	//getting magic number
+	c = fgetc(input_fp);
+	if(c != 51 && c != 54){
+		printf("Incorrect file type: Expected P3 or P6 file\n");
+		return 1;
 	}
-	fwrite(buffer, size, 1, stdout);
+	
+	if(c == 51){
+		//send fp to conversion function
+	}
+	if(c == 54){
+		p6_to_p3(input_fp, output_fp, buffer);
+	}
 	
 	return 0;
+}
+
+void p6_to_p3(FILE *input, FILE *output, Pixel *buffer){
+	int c;
+	c = fgetc(input);
+	if(c != '\n'){
+		printf("Incorrect .PPM file type\n");
+		EXIT_FAILURE;
+	}
+	
+	// placing P3 header info into new file
+	c = fgetc(input);
+	fprintf(output, "P3\n");
+	input = escape_comments(input, c);
+	
+	// transfering image dimension header info to new file
+	fscanf(input, "\n%d %d\n%d\n", &width, &height, &depth);
+	fprintf(output, "%d %d\n%d\n", width, height, depth);
+	
+	// generating pixel buffer
+	Pixel new;
+	fread(&new.red, 1, 1, input);
+	fread(&new.green, 1, 1, input);
+	fread(&new.blue, 1, 1, input);
+	fprintf(output, "%d %d %d", new.red, new.green, new.blue);
+}
+
+FILE* escape_comments(FILE *input, int c){
+	if(c == '#'){
+		while(c != '\n'){
+			printf("traversing comments...\n");
+			c = fgetc(input);
+		}
+		printf("escaped comments\n");
+		//c = fgetc(input);
+		input = escape_comments(input, c);
+	}
+	return input;
 }
